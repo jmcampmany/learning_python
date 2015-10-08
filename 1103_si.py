84f1ced327fa6a59c6afd0fa56718fe0c9edda4d
Juan Manuel Benítez <jmcampmany@gmail.com>
2015-10-08 21:43:01 +0200

    salida cadena

--- 1103_si.py
+++ 1103_si.py
@@ -2,14 +2,9 @@
 #-*- coding: utf-8 -*-
 def mayor(n1, n2):
 	if n1 > n2:
-		 salida = "a es mayor que b"
-		 salida = salida + "insisto a es mayor que b"
+		 salida = True
 	else:
-		salida = "b es mayor que a"
-		salida +=" o igual"
-	print "esto lo va a pintar de todas maneras"
-	return salida
-	
+		salida = False
 	
 
 
