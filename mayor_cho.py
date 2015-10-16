21f796bb2787cf3ae742654024136cc9c194a8bc
jmcampmany <jmcampmany@gmail.com>
2015-10-16 20:01:23 +0200

    comentarios añadidos

--- mayor_cho.py
+++ mayor_cho.py
@@ -5,12 +5,12 @@
 num = int(raw_input("Introduce números para saber el mayor de ellos. Para salir introduce un negativo: "))
 mayor = num
 
-if num < 0:
-	print "Acabas de empezar y ya te has aburrido"
-else:
+if num < 0: #Si el número introducido es negativo sale del programa despidiéndose.
+	print "Acabas de empezar y ya te has aburrido. Adiós."
+else:#Si no lo es recoge números y almacena el que vaya siendo mayor en "mayor" hasta que se introduzca un negativo.
 	while num >= 0:
 		if num > mayor:
 			mayor = num
 		num = int(raw_input("Introduce números para saber el mayor de ellos. Para salir introduce un negativo: "))
-	print "El mayor número es: " + str(mayor)
+	print "El mayor número es: " + str(mayor) + ". Adiós."
 
